9b6949f561a170029b677cc6fe7b58b91851a82b
xdelmo <emanuele.delmo@gmail.com>
2024-04-24 16:50:52 +0200

    Layout challenge

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,8 +11,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+              child: Text("Container 1"),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.teal,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+              child: Text("Container 3"),
+            ),
+          ],
+        )),
       ),
     );
   }
