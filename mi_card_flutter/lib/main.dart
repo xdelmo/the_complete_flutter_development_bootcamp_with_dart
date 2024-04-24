32701e499428a1e402ee15fe71a2e5a65c18584d
xdelmo <emanuele.delmo@gmail.com>
2024-04-24 17:50:30 +0200

    Final card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,55 +13,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/avatar.png'),
-              ),
-              Text(
-                'Emanuele Del Monte',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico'),
-              ),
-              Text(
-                'JUNIOR FRONTEND DEVELOPER',
-                style: TextStyle(
-                    fontSize: 10,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/avatar.png'),
+                ),
+                Text(
+                  'Emanuele Del Monte',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'),
+                ),
+                Text(
+                  'JUNIOR FRONTEND DEVELOPER',
+                  style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Source Code Pro',
+                      letterSpacing: 2.5),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
                     color: Colors.teal.shade100,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Source Code Pro',
-                    letterSpacing: 2.5),
-              ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Icon(
+                  ),
+                ),
+                Card(
+                  elevation: 10.0,
+                  shadowColor: Colors.teal.shade900,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
                       Icons.smartphone,
                       color: Colors.teal,
                     ),
-                    SizedBox(width: 10),
-                    Text(
+                    title: Text(
                       '+39 38099999999',
                       style: TextStyle(
                           color: Colors.teal.shade900,
                           fontFamily: 'Source Code Pro',
                           fontSize: 15),
-                    )
-                  ],
+                    ),
+                  ),
                 ),
-              )
-            ],
+                Card(
+                  elevation: 10.0,
+                  shadowColor: Colors.teal.shade900,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'info@emanueledelmonte.it',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 15),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        )),
+        ),
       ),
     );
   }
