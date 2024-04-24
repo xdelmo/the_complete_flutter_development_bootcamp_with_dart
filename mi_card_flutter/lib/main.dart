f46a2e27c5861616794ed453e97889656b4a537d
xdelmo <emanuele.delmo@gmail.com>
2024-04-24 17:51:50 +0200

    color fix

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: SafeArea(
           child: Center(
             child: Column(
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
                   'JUNIOR FRONTEND DEVELOPER',
                   style: TextStyle(
                       fontSize: 10,
-                      color: Colors.teal.shade100,
+                      color: Colors.blue.shade100,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Source Code Pro',
                       letterSpacing: 2.5),
@@ -42,22 +42,22 @@ class MyApp extends StatelessWidget {
                   height: 20,
                   width: 150,
                   child: Divider(
-                    color: Colors.teal.shade100,
+                    color: Colors.blue.shade100,
                   ),
                 ),
                 Card(
                   elevation: 10.0,
-                  shadowColor: Colors.teal.shade900,
+                  shadowColor: Colors.blue.shade900,
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                   child: ListTile(
                     leading: Icon(
                       Icons.smartphone,
-                      color: Colors.teal,
+                      color: Colors.blue,
                     ),
                     title: Text(
                       '+39 38099999999',
                       style: TextStyle(
-                          color: Colors.teal.shade900,
+                          color: Colors.blue.shade900,
                           fontFamily: 'Source Code Pro',
                           fontSize: 15),
                     ),
@@ -65,17 +65,17 @@ class MyApp extends StatelessWidget {
                 ),
                 Card(
                   elevation: 10.0,
-                  shadowColor: Colors.teal.shade900,
+                  shadowColor: Colors.blue.shade900,
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                   child: ListTile(
                     leading: Icon(
                       Icons.email,
-                      color: Colors.teal,
+                      color: Colors.blue,
                     ),
                     title: Text(
                       'info@emanueledelmonte.it',
                       style: TextStyle(
-                          color: Colors.teal.shade900,
+                          color: Colors.blue.shade900,
                           fontFamily: 'Source Code Pro',
                           fontSize: 15),
                     ),
