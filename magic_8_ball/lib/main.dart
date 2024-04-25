c85c4787adf6151dcbd70ef8415e9e37c4b703ce
xdelmo <emanuele.delmo@gmail.com>
2024-04-25 19:30:06 +0200

    magic 8 ball app

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,7 +1,56 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Text("magic 8 ball"),
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade500,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+        titleTextStyle: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: TextButton(
+          onPressed: () => (setState(() {
+            ballNumber = getRandomNumber();
+          })),
+          child: Image.asset("images/ball$ballNumber.png"),
+        ),
+      ),
+    ]);
+  }
+}
+
+int getRandomNumber() {
+  return Random().nextInt(5) + 1;
+}
