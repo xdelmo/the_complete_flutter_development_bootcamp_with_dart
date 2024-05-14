4a2e22a6388047d5d2bdecec7e27897a3d87e72b
xdelmo <emanuele.delmo@gmail.com>
2024-05-14 18:09:48 +0200

    input page

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
@@ -11,25 +12,6 @@ class BMICalculator extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
     );
   }
 }
