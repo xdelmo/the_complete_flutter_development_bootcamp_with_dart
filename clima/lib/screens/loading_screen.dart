0c7dab1a0c3f1a752ff793ccebf179c418702795
xdelmo <emanuele.delmo@gmail.com>
2024-05-21 16:40:05 +0200

    ad geolocator package and fix compatibility

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+void getLocation() async {
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low);
+  print(position);
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
@@ -12,7 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
