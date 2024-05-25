aeff90621e12c9bcdc2bf557c73678946812f2ea
xdelmo <emanuele.delmo@gmail.com>
2024-05-25 16:58:45 +0200

    jsonDecode

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -27,7 +28,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+      final decodeData = jsonDecode(data);
+
+      String weatherDescription = decodeData['weather'][0]['description'];
+      double temp = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
     } else {
       print(response.statusCode);
     }
