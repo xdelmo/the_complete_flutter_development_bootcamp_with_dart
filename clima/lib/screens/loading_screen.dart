7caf31e9b14a61d0da02a8bc20c995876f9f539a
xdelmo <emanuele.delmo@gmail.com>
2024-05-25 16:51:15 +0200

    GET api

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,17 +12,33 @@ class _LoadingScreenState extends State<LoadingScreen> {
   initState() {
     super.initState();
     getLocation();
+    // getData();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'
+            as Uri);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('Get Location'),
+      ),
+    );
   }
 }
