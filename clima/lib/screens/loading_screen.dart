c3fd7f80c741d9fdfa582ca833f1582786cacc9d
xdelmo <emanuele.delmo@gmail.com>
2024-05-26 12:54:10 +0200

    spinner

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
@@ -29,6 +30,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
           'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
       var weatherData = await networkHelper.getData();
       print(weatherData);
+
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return LocationScreen();
+      }));
     } catch (e) {
       print('An error occurred: $e');
     }
@@ -38,7 +43,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Get Location'),
+        child: CircularProgressIndicator(
+          color: Colors.white,
+        ),
       ),
     );
   }
