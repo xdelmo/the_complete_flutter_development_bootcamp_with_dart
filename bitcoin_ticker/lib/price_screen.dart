3cd45e06532b6cdba4c5db0a816156c3c8dd9c06
xdelmo <emanuele.delmo@gmail.com>
2024-06-04 17:09:32 +0200

    fix

--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -95,7 +95,7 @@ class _PriceScreenState extends State<PriceScreen> {
               alignment: Alignment.center,
               padding: EdgeInsets.only(bottom: 30.0),
               color: Colors.lightBlue,
-              child: getPicker()),
+              child: getAndroidDropdown()),
         ],
       ),
     );
