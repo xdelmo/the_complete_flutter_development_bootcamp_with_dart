1ceda7d9df65d13464205ce27de4866d0bfe8f24
xdelmo <emanuele.delmo@gmail.com>
2024-06-23 16:40:55 +0200

    firebase on ios

--- flash_chat/lib/main.dart
+++ flash_chat/lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
-void main() => runApp(const FlashChat());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   const FlashChat({super.key});
