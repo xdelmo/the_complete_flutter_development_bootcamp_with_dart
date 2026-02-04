d6b288f86217fd9a5831a950da53ee9d24770719
xdelmo <emanuele.delmo@gmail.com>
2026-02-04 20:28:46 +0100

    feat: Introduce Task model and refactor `TaskTile` and `TasksList` to manage and display dynamic task data.

--- todoey/lib/widgets/task_tile.dart
+++ todoey/lib/widgets/task_tile.dart
@@ -1,42 +1,39 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  const TaskTile({super.key});
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function(bool?) checkboxCallback;
 
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = true;
+  const TaskTile({
+    super.key,
+    required this.isChecked,
+    required this.taskTitle,
+    required this.checkboxCallback,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          "New Task",
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
-        trailing: TaskCheckbox(isChecked, (bool? checkboxState) {
-          setState(() {
-            isChecked = checkboxState ?? false;
-          });
-        }));
-  }
-}
-
-class TaskCheckbox extends StatelessWidget {
-  final bool checkboxState;
-  final Function(bool?) toggleCheckBoxState;
-
-  const TaskCheckbox(this.checkboxState, this.toggleCheckBoxState, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
+      ),
+      trailing: Checkbox(
         activeColor: Colors.lightBlueAccent,
-        value: checkboxState,
-        onChanged: toggleCheckBoxState);
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
+    );
   }
 }
+
+
+
+// (bool? checkboxState) {
+//           setState(() {
+//             isChecked = checkboxState ?? false;
+//           });
+//         }
--- todoey/lib/widgets/tasks_list.dart
+++ todoey/lib/widgets/tasks_list.dart
@@ -1,22 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/task_tile.dart';
+import '../models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk', isDone: false),
+    Task(name: 'Buy eggs', isDone: false),
+    Task(name: 'Buy bread', isDone: false),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.only(
-        top: 25.0,
-        left: 25.0,
-        right: 25.0,
-      ),
-      children: const <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
-    );
+    return ListView.builder(
+        itemBuilder: (context, index) {
+          return TaskTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallback: (bool? checkboxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            },
+          );
+        },
+        itemCount: tasks.length);
   }
 }
+
+    
+    
+    // ListView(
+    //   padding: const EdgeInsets.only(
+    //     top: 25.0,
+    //     left: 25.0,
+    //     right: 25.0,
+    //   ),
+    //   children: <Widget>[
+    //     TaskTile(
+    //       taskTitle: tasks[0].name,
+    //       isChecked: tasks[0].isDone,
+    //       checkboxCallback: (bool? checkboxState) {
+    //         setState(() {
+    //           tasks[0].isDone = checkboxState ?? false;
+    //         });
+    //       },
+    //     ),
+    //     TaskTile(
+    //       taskTitle: tasks[1].name,
+    //       isChecked: tasks[1].isDone,
+    //       checkboxCallback: (bool? checkboxState) {
+    //         setState(() {
+    //           tasks[1].isDone = checkboxState ?? false;
+    //         });
+    //       },
+    //     ),
+    //     TaskTile(
+    //       taskTitle: tasks[2].name,
+    //       isChecked: tasks[2].isDone,
+    //       checkboxCallback: (bool? checkboxState) {
+    //         setState(() {
+    //           tasks[2].isDone = checkboxState ?? false;
+    //         });
+    //       },
+    //     ),
+    //   ],
+    // );
+    
