2cfe7263a0dec20889822f038e79e47dfa0c16cf
xdelmo <emanuele.delmo@gmail.com>
2026-02-04 19:24:29 +0100

    feat: Convert TaskTile to StatefulWidget and refactor checkbox into a separate widget to manage task completion state.

--- todoey/lib/widgets/task_tile.dart
+++ todoey/lib/widgets/task_tile.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({super.key});
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("data"),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {
-          value != value;
-        },
-      ),
-    );
+        title: Text(
+          "New Task",
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: TaskCheckbox(isChecked, (bool? checkboxState) {
+          setState(() {
+            isChecked = checkboxState ?? false;
+          });
+        }));
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function(bool?) toggleCheckBoxState;
+
+  const TaskCheckbox(this.checkboxState, this.toggleCheckBoxState, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: checkboxState,
+        onChanged: toggleCheckBoxState);
   }
 }
